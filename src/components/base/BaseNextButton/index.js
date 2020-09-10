553d83196856e55396ed8280dba4c1aaf20ac2ca
Zhenya <klisk777@gmail.com>
2020-09-10 10:47:19 +0300

    base Next button component done

---
+++ src/components/base/BaseNextButton/index.js
@@ -0,0 +1,34 @@
+// Core
+import styled from "styled-components";
+import { fonts } from "../../../theming";
+
+const BaseNextButton = styled.button`
+  height: 40px;
+  background-color: ${({ background, disabled }) =>
+    disabled ? "#7C7C89" : background ? background : "#000000"};
+  color: ${({ textColor }) => (textColor ? textColor : "#FFFFFF")};
+  padding: 12px 25px 12px 34px;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  ${fonts.archivoBlack(14, 15)};
+  position: relative;
+  :after {
+    position: absolute;
+    right: -20px;
+    top: 0;
+    content: "";
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 20px 0 20px 20px;
+    border-color: ${({ background, disabled }) =>
+      disabled
+        ? "transparent transparent transparent #7C7C89"
+        : background
+        ? background
+        : "transparent transparent transparent #000000"};
+  }
+`;
+
+export default BaseNextButton;

3a9fce0530802650e083e4be0050df20c12ddba4
Urbana28 <a.yarosh@nord-soft.com>
2020-09-17 11:45:07 +0300

    icons

--- src/_UI/Pages/ActivateCompany/AdditionalUser/UserCompleteForm.tsx
+++ src/_UI/Pages/ActivateCompany/AdditionalUser/UserCompleteForm.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {FillOuter, FormContainer, FullfilledWrap, Label, LineWrap, TextWrap } from './additional-user-styles'
 
 
 const UserCompleteForm:React.FC = () => {
     return (
-        <div></div>
+        <FormContainer>
+            <FullfilledWrap>
+                <FillOuter>
+                    <Label>Email</Label>
+                    <TextWrap>blabla@blatrteerterertertt</TextWrap>
+                </FillOuter>
+                <FillOuter>
+                    <Label>Roles</Label>
+                    <TextWrap>fdf, 454</TextWrap>
+                </FillOuter>
+            </FullfilledWrap>
+            <LineWrap/>
+        </FormContainer>
     )
 }
 
--- src/_UI/Pages/ActivateCompany/AdditionalUser/additional-user-styles.ts
+++ src/_UI/Pages/ActivateCompany/AdditionalUser/additional-user-styles.ts
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div`
 width: 100%;
-min-height: 100vh;
-height: 100%;
+
+height: 100vh;
 display: flex;
 justify-content: center;
 `
@@ -20,4 +20,43 @@ export const Title = styled.div`
 font-family: "Helvetica Bold", sans-serif;
 font-size: 28px;
 color: #1B1B25;
+margin-bottom: 50px;
+`
+export const FormContainer = styled.form`
+max-width: 420px;
+width: 100%;
+height: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+export const FullfilledWrap = styled.div`
+width: 100%;
+display: flex;
+justify-content: space-between;
+margin-bottom: 27px;
+`
+export const LineWrap = styled.div`
+width: 100%;
+height: 1px;
+background-color: #E0E0E0;
+`
+export const FillOuter = styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+max-width: 190px;
+width: 100%;
+
+`
+export const Label = styled.div`
+margin-bottom: 10px;
+font-family: "Helvetica Bold", sans-serif;
+color: black;
+font-size: 14px;
+`
+export const TextWrap = styled.div`
+color: #1B1B25;
+font-family: "Helvetica Light", sans-serif;
+font-size: 16px;
 `

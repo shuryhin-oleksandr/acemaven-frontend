982086860465bfb407571e74e7fa50ab72f5e726
Zhenya <klisk777@gmail.com>
2020-10-05 15:55:54 +0300

    sign out functionality done

--- src/_UI/components/Header/Header.tsx
+++ src/_UI/components/Header/Header.tsx
@@ -1,43 +1,108 @@
 import * as React from "react";
-import {ButtonWrap, HeaderContainer,  LogoWrap, PhotoWrap} from "./header-styles";
-import {useDispatch, useSelector} from "react-redux";
-import {AppStateType} from "../../../_BLL/store";
+import {
+  ButtonWrap,
+  HeaderContainer,
+  LogoWrap,
+  PhotoWrap,
+} from "./header-styles";
+import { useDispatch, useSelector } from "react-redux";
+import { AppStateType } from "../../../_BLL/store";
 import styled from "styled-components";
-import {useEffect} from "react";
-import {getAuthUserInfo} from "../../../_BLL/reducers/profileReducer";
-const notification = require('../../../_UI/assets/icons/clarity_notification-solid-badged.svg') as string;
-const card = require('../../../_UI/assets/icons/card.svg') as string;
-const user = require('../../../_UI/assets/icons/profile/defaultUserPhoto.svg')
+import { useEffect } from "react";
+import { getAuthUserInfo } from "../../../_BLL/reducers/profileReducer";
+import { signOut } from "../../../_BLL/reducers/authReducer";
+import Tooltip from "@material-ui/core/Tooltip";
+import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router";
+const notification = require("../../../_UI/assets/icons/clarity_notification-solid-badged.svg") as string;
+const card = require("../../../_UI/assets/icons/card.svg") as string;
+const user = require("../../../_UI/assets/icons/profile/defaultUserPhoto.svg");
 
+const useStyles = makeStyles({
+  customTooltip: {
+    "& .MuiTooltip-arrow::before": {
+      backgroundColor: "#FFFFFF",
+      border: "1px solid #828282",
+    },
+    borderRadius: "4px",
+    boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.25)",
+    backgroundColor: "#FFFFFF",
+    border: "1px solid #828282",
+    padding: "20px 25px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
+const Header: React.FC = () => {
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const history = useHistory();
 
-const Header:React.FC = () => {
-    const dispatch = useDispatch()
+  useEffect(() => {
+    localStorage.getItem("access_token") && dispatch(getAuthUserInfo());
+  }, []);
 
-    useEffect(() => {
-       localStorage.getItem('access_token') && dispatch(getAuthUserInfo())
-    }, [])
+  let profilePhoto = useSelector(
+    (state: AppStateType) => state.profile.authUserInfo?.photo
+  );
 
-    let profilePhoto = useSelector((state: AppStateType) => state.profile.authUserInfo?.photo)
+  return (
+    <HeaderContainer>
+      <LogoWrap>ACEMAVEN</LogoWrap>
+      <Info>
+        <ButtonWrap>
+          <img src={card} alt="" />
+        </ButtonWrap>
+        <ButtonWrap>
+          <img src={notification} alt="" />
+        </ButtonWrap>
+        <Tooltip
+          arrow
+          interactive
+          classes={{ tooltip: classes.customTooltip }}
+          title={
+            <StyledUl>
+              <StyledTooltipItem onClick={() => dispatch(signOut(history))}>
+                Log out
+              </StyledTooltipItem>
+              <StyledTooltipItem>My profile</StyledTooltipItem>
+            </StyledUl>
+          }
+        >
+          <PhotoWrap>
+            <img src={profilePhoto ? profilePhoto : user} alt="" />
+          </PhotoWrap>
+        </Tooltip>
+      </Info>
+    </HeaderContainer>
+  );
+};
 
-    return (
-        <HeaderContainer>
-            <LogoWrap>ACEMAVEN</LogoWrap>
-             <Info>
-                <ButtonWrap><img src={card} alt=""/></ButtonWrap>
-                <ButtonWrap><img src={notification} alt=""/></ButtonWrap>
-                <PhotoWrap><img src={profilePhoto? profilePhoto : user} alt=""/></PhotoWrap>
-            </Info>
-
-        </HeaderContainer>
-    )
-}
-
-export default Header
+export default Header;
 
 const Info = styled.div`
   max-width: 115px;
   width: 100%;
   display: flex;
-justify-content: space-between;
-`
+  justify-content: space-between;
+`;
+
+const StyledUl = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const StyledTooltipItem = styled.li`
+  font-family: "Helvetica Bold", sans-serif;
+  cursor: pointer;
+  font-size: 18px;
+  line-height: 21px;
+  color: #1b1b25;
+  margin-bottom: 15px;
+  :last-child {
+    margin-bottom: 0;
+  }
+`;

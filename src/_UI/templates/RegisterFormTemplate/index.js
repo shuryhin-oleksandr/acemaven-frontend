627438b637e1e0386b2c5d48fbac82318baff427
Zhenya <klisk777@gmail.com>
2020-10-05 16:42:03 +0300

    close sign in popup button position fix

--- src/_UI/templates/RegisterFormTemplate/index.js
+++ src/_UI/templates/RegisterFormTemplate/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import styled from "styled-components";
-import close_icon from '../../../_UI/assets/icons/close-icon.svg'
+import close_icon from "../../../_UI/assets/icons/close-icon.svg";
 
 const RegisterFormTemplate = ({ children, openFlow }) => {
   return (
     <Container>
-      <ContentWrapper>{children}
-          <CloseIcon onClick={openFlow}><img src={close_icon} alt=""/></CloseIcon>
+      <ContentWrapper>
+        {children}
+        <CloseIcon onClick={openFlow}>
+          <img src={close_icon} alt="" />
+        </CloseIcon>
       </ContentWrapper>
     </Container>
   );
@@ -33,17 +36,17 @@ const ContentWrapper = styled.div`
   padding: 40px 140px;
   box-shadow: 0 0 15px rgba(0, 0, 0, 0.25);
   background-color: #ffffff;
- position: relative;
+  position: relative;
 `;
 const CloseIcon = styled.button`
-    outline: none;
-    border: none;
-    background: none;
-    right: 2%;
-    position: absolute;
-    top: 6%;
-    
-    &:hover {
-      cursor: pointer;
-    }
-`
+  outline: none;
+  border: none;
+  background: none;
+  right: 20px;
+  position: absolute;
+  top: 25px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;

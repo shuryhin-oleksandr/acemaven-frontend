dfca5efb89e4a43c28b77866aac76118b22a11c0
viacheslav <v.bogdanov@nord-soft.com>
2020-11-10 15:08:19 +0300

    correct container in sign in/up container

--- src/_UI/Pages/landing/landing-styles.ts
+++ src/_UI/Pages/landing/landing-styles.ts
@@ -3,7 +3,8 @@ import styled from 'styled-components'
 
 export const LandingContainer = styled.div`
   width: 100%;
-height: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
+  position: relative;
 `

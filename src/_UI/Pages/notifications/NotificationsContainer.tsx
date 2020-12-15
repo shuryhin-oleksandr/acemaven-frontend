eb23dfcbca4f8965ea0d71f545db1896e67df236
Urbana28 <a.yarosh@nord-soft.com>
2020-12-15 20:57:51 +0300

    added notifications page (draft one)

---
+++ src/_UI/Pages/notifications/NotificationsContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from "../../components/BaseLayout/Layout";
+import NotificationsPage from "./NotificationsPage";
+
+const NotificationsContainer:React.FC = () => {
+    return (
+        <Layout>
+            <NotificationsPage />
+        </Layout>
+    )
+}
+
+export default NotificationsContainer
---
+++ src/_UI/Pages/notifications/notifications-style.ts
@@ -0,0 +1,64 @@
+import styled from 'styled-components'
+
+export const NotificationsWrapper = styled.div`
+width: 100%;
+height: 100%;
+`
+export const NotificationsInner = styled.div`
+width: 100%;
+height: 100%;
+padding: 50px 50px 50px 30px;
+`
+export const NotificationsContent = styled.div`
+ display: flex;
+ flex-direction: column;
+ align-items: flex-start;
+`
+export const NotificationsTitle = styled.div`
+  font-family: "Helvetica Bold", sans-serif;
+  font-size: 48px;
+  color: black;
+  margin-bottom: 22px;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`
+export const SectionWrapper =  styled.div`
+  width: 100%;
+`
+export const CardOuter = styled.div`
+  border-bottom: 1px solid #E0E0E0;
+  padding-bottom: 15px;
+  display: flex;
+  margin-bottom: 36px;
+`
+export const IconWrapper = styled.div`
+  margin-right: 10px;
+  img {
+    box-shadow: 3px 2px 4px rgba(0,0,0,0.15);
+  }
+`
+export const BlockWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`
+export const NotificationsBlock = styled.div`
+  font-family: "Helvetica Bold", sans-serif;
+  font-size: 16px;
+  color: black;
+  margin-bottom: 5px;
+`
+export const NotificationsDate = styled.div`
+  font-family: "Helvetica Light", sans-serif;
+  font-size: 14px;
+  color: #828282;
+  margin-bottom: 5px;
+ 
+`
+export const NotificationsDescription = styled.div`
+  font-family: "Helvetica Reg", sans-serif;
+  font-size: 14px;
+  color: #333333;
+`

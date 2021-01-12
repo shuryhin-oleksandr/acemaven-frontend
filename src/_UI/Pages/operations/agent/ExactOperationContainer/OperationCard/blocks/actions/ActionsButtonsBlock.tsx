5f3666a9a3e7717be50b71c2382179662527cf00
Zhenya <klisk777@gmail.com>
2021-01-12 10:51:54 +0300

    has_review flag added

--- src/_UI/Pages/operations/agent/ExactOperationContainer/OperationCard/blocks/actions/ActionsButtonsBlock.tsx
+++ src/_UI/Pages/operations/agent/ExactOperationContainer/OperationCard/blocks/actions/ActionsButtonsBlock.tsx
@@ -37,7 +37,7 @@ const ActionsButtonsBlock: React.FC<PropsType> = ({operation_info, my_name, comp
         <ActionsButtons>
             {company_type?.type === AppCompaniesTypes.CLIENT
             && operation_info?.status === AppOperationBookingStatusesType.COMPLETED
-            &&
+            && !operation_info?.has_review &&
             <ConfirmButton onClick={() => setReviewPopup(true)}>
                 Leave a review
             </ConfirmButton>

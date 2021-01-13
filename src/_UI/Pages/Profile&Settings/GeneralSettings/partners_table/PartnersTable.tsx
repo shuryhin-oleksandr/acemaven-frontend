ef16742c37b85003d84fc72e8b7631ceee76032e
Zhenya <klisk777@gmail.com>
2021-01-13 11:26:00 +0300

    partners table in profile settings connected to api

--- src/_UI/Pages/Profile&Settings/GeneralSettings/partners_table/PartnersTable.tsx
+++ src/_UI/Pages/Profile&Settings/GeneralSettings/partners_table/PartnersTable.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import PartnersCard from './PartnersCard';
+import React, { useEffect } from "react";
+import PartnersCard from "./PartnersCard";
+import { useDispatch, useSelector } from "react-redux";
+import { getPartnersThunk } from "../../../../../_BLL/thunks/search_client_thunks/searchClientThunks";
+import { AppStateType } from "../../../../../_BLL/store";
+import ScrollbarStyled from "../../../../components/_commonComponents/ScrollbarStyled/ScrollbarStyled";
 
-type PropsType = {
+type PropsType = {};
 
-}
+const PartnersTable: React.FC<PropsType> = () => {
+  let dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPartnersThunk());
+  }, []);
 
+  let partners = useSelector((state: AppStateType) => state.search.partners);
 
-const PartnersTable:React.FC<PropsType> = ({}) => {
-    return (
-        <PartnersCard />
-    )
-}
+  return (
+    <ScrollbarStyled
+      {...{
+        style: { width: "100%" },
+        autoHeightMin: 419,
+        autoHeightMax: "calc(100vh - 341px)",
+        autoHeight: true,
+      }}
+    >
+      {partners.map((p) => (
+        <PartnersCard partner={p} key={p.id} />
+      ))}
+    </ScrollbarStyled>
+  );
+};
 
-export default PartnersTable
+export default PartnersTable;
--- src/_UI/Pages/Profile&Settings/GeneralSettings/partners_table/partenrs-table-style.ts
+++ src/_UI/Pages/Profile&Settings/GeneralSettings/partners_table/partenrs-table-style.ts
@@ -1,24 +1,25 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const PartnersCardOuter = styled.div`
   max-width: 1089px;
   min-width: 350px;
   width: 100%;
-  border: 1px solid #ECECEC;
+  border: 1px solid #ececec;
   box-shadow: 0 0 7px rgba(0, 0, 0, 0.15);
-`
+  margin-bottom: 30px;
+`;
 export const CardContent = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
   height: 100%;
   padding: 20px;
-`
+`;
 export const SpanWrap = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 6px;
-`
+`;
 export const LabelWrap = styled.div`
   color: black;
   font-family: "Helvetica Reg", sans-serif;
@@ -29,10 +30,10 @@ export const LabelWrap = styled.div`
   width: 100%;
   display: flex;
   justify-content: flex-end;
-`
+`;
 export const TextWrap = styled.div`
-   color: #4F4F4F;
-   font-family: "Helvetica Reg", sans-serif;
+  color: #4f4f4f;
+  font-family: "Helvetica Reg", sans-serif;
   font-size: 16px;
   text-align: left;
-`
+`;

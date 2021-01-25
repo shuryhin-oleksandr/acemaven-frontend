36301f4451b75fa967c5b96bd6bd3357756952df
Zhenya <klisk777@gmail.com>
2021-01-25 14:24:46 +0300

    added helper function to billing in progress

--- src/_BLL/helpers/tracker/autoTracksWithEventsHelper.ts
+++ src/_BLL/helpers/tracker/autoTracksWithEventsHelper.ts
@@ -1,13 +1,12 @@
 import {ShippingTypesEnum} from "../../types/rates&surcharges/newSurchargesTypes";
 import {OperationType} from "../../types/operations/operationsTypes";
 
-export const autoTrackWithEventsHelper = (operations_list: OperationType[]) => {
-    let operations_with_auto_tracking = operations_list.filter(o => o.tracking?.length > 0 && o.automatic_tracking)
-    let operations_for_drawing_on_map = operations_with_auto_tracking.filter(o => (o.shipping_type === ShippingTypesEnum.AIR)
+export const autoTrackWithEventsHelper = (operations_list: any) => {
+    let operations_with_auto_tracking = operations_list.filter((o:any) => o.tracking?.length > 0 && o.automatic_tracking)
+    let operations_for_drawing_on_map = operations_with_auto_tracking.filter((o:any) => (o.shipping_type === ShippingTypesEnum.AIR)
         ? o.tracking[0].data.events?.length > 0 && o
         : o.tracking[0].data.data.locations?.length > 0 && o)
-
-    let events = operations_for_drawing_on_map.map(o => ({
+    let events = operations_for_drawing_on_map.map((o:any) => ({
         ...o.tracking_initial,
         locations: o.shipping_type === ShippingTypesEnum.AIR
             ? o.tracking?.map((ot: any) => ot?.data?.events.map((e: any) => ({

ced16fd11fdc384c6ecc3ac01d3a8d2cd7f93f01
Zhenya <klisk777@gmail.com>
2021-01-29 14:14:40 +0300

    svg fix

--- src/_BLL/helpers/tracker/BillingMapHelper.ts
+++ src/_BLL/helpers/tracker/BillingMapHelper.ts
@@ -35,10 +35,10 @@ export const polylineBillingMapIcons = (props: InterfaceFuncIcons) => {
     {
       icon: {
         path:
-          "M8 -1C8 3.97056 3.97056 8 -1 8C-5.97056 8 -10 3.97056 -10 -1C-10 -5.97056 -5.97056 -10 -1 -10C3.97056 -10 8 -5.97056 8 -1Z",
-        strokeColor: "#F86565",
+            "M-10 -23C-17.1797 -23 -23 -17.1797 -23 -10C-23 -2.8203 -17.1797 3 -10 3C-2.8203 3 3 -2.8203 3 -10C3 -17.1797 -2.8203 -23 -10 -23ZM-25 -10C-25 -18.2843 -18.2843 -25 -10 -25C-1.71573 -25 5 -18.2843 5 -10C5 -1.71573 -1.71573 5 -10 5C-18.2843 5 -25 -1.71573 -25 -10Z" ,
+        strokeColor: "#white",
         strokeOpacity: 1,
-        fillColor: "#F86565",
+        fillColor: "white",
         fillOpacity: 1,
       },
       fixedRotation: true,
@@ -47,10 +47,10 @@ export const polylineBillingMapIcons = (props: InterfaceFuncIcons) => {
     {
       icon: {
         path:
-          "M8 -1C8 3.97056 3.97056 8 -1 8C-5.97056 8 -10 3.97056 -10 -1C-10 -5.97056 -5.97056 -10 -1 -10C3.97056 -10 8 -5.97056 8 -1Z",
-        strokeColor: "blue",
+            "M4 -5C4 -0.0294371 -0.0294371 4 -5 4C-9.97056 4 -14 -0.0294371 -14 -5C-14 -9.97056 -9.97056 -14 -5 -14C-0.0294371 -14 4 -9.97056 4 -5Z",
+        strokeColor: "#FFC8C8",
         strokeOpacity: 1,
-        fillColor: "blue",
+        fillColor: "#FFC8C8",
         fillOpacity: 1,
       },
       fixedRotation: true,

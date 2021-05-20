ff6a7ab4afdcb90d4698cdba1da284e2a129f337
Artem Kotov <art.kotov1@gmail.com>
2021-05-20 16:20:12 +0300

    update packages.json

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { authActions } from "./_BLL/reducers/authReducer";
 import Spinner from "./_UI/components/_commonComponents/spinner/Spinner";
 import { Scrollbars } from "react-custom-scrollbars";
 import {changeLanguageAtBackEnd, getAuthUserInfo} from "./_BLL/thunks/profile/profileThunks";
-import { wsChatHelper } from "./_BLL/helpers/wsChatHelper";
 import {
   startReceiveNotifications,
   stopReceiveNotifications,
@@ -14,7 +13,6 @@ import {
   startReceiveChatNotifications,
   stopReceiveChatNotifications,
 } from "./_BLL/thunks/chat_notifications_thunk/chat_notifications_thunk";
-import { profileActions } from "./_BLL/reducers/profileReducer";
 import i18next from "i18next";
 import {getMyInfoSelector} from "./_BLL/selectors/profile/profileSelectors";
 
@@ -26,8 +24,6 @@ function App() {
   const dispatch = useDispatch();
   let token = localStorage.getItem("access_token");
   let language = localStorage.getItem('language');
-  // let languageFromBack = useSelector((state) => state.profile.authUserInfo.language);
-  // console.log("languageFromBack ", languageFromBack)
 
   useEffect(() => {
     if (token) {
@@ -73,11 +69,6 @@ function App() {
       )}
     >
       <div className="App">{route}</div>
-      {/*<div style={{ position: "fixed", left:0, bottom:0, right:0, backgroundColor:"lightgrey", zIndex:100 }}>*/}
-      {/*  <button onClick={() => i18next.changeLanguage("en")}>EN</button>*/}
-      {/*  <button onClick={() => i18next.changeLanguage("por")}>POR</button>*/}
-      {/*  <button onClick={() => i18next.changeLanguage("spa")}>SPA</button>*/}
-      {/*</div>*/}
     </Scrollbars>
   ) : (
     <Spinner />
